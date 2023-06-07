0f3b1c927085faa2dc1f8b9c23412032bb3a8a8a
Yim <yimjh2309@gmail.com>
2023-06-07 15:26:59 +0900

    12~14장 업데이트

---
+++ JavaScript/JS_Study/9-장/test.js
@@ -0,0 +1,23 @@
+var x = "global x";
+var y = "global y";
+
+function outer() {
+   var z = "outer's local z";
+   
+   console.log(x); // global x
+   console.log(y); // global y
+   console.log(z); // outer's local z
+
+   function inner() {
+      var x = "inner's local x";
+   
+   console.log(x); // inner's local x
+   console.log(y); // global y
+   console.log(z); // outer's local z
+   }
+   inner();
+}
+outer();
+
+console.log(x); // global x
+console.log(z); // 정의되지 않음.
