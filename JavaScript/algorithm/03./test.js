3ea341c5f01c99e5f7248cdd2c0d1f76fe16e965
Hyurim <kyo2301@naver.com>
2023-08-10 18:57:04 +0900

    해쉬 테이블 정답 추가

--- JavaScript/algorithm/03./test.js
+++ JavaScript/algorithm/03./test.js
@@ -1,12 +1,30 @@
-let genres = ["classic", "pop", "classic", "classic", "pop"];
-let plays = [500, 600, 150, 800, 2500];
+const genres = ["classic", "pop", "classic", "classic", "pop"];
+const plays = [500, 600, 150, 800, 2500];
 
-// let genresSet = new Set(genres);
-// console.log(genresSet.values());
+let sing = [];
+let array1 =[];
 
-const song = new Map([]);
+const genre1 = new Set(genres);
+console.log(genre1);
 
 for(let a = 0; a < genres.length; a++){
-    song.set[genres[a], plays[a]];
-    console.log(song.values());    
+    sing.push({genre : genres[a], play : plays[a]});
 }
+
+sing.sort((b,c) => {
+    if (c.genre === b.genre){
+        return c.play - b.play;
+    }
+    else{
+        return b.genre.localeCompare(c.genre)
+    }}
+    );
+console.log(sing[0]);
+
+for(let aa = 0; aa < genre1; aa++) {
+    if(sing.includes(genre1[aa]) == true) {
+        a
+
+    }
+}
+
